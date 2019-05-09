b271131109b6968b11faa7205c11733f394b2841
Jeremy Jones <jeremy.jonze@gmail.com>
2019-05-09 09:06:40 -0400

    dep script

---
+++ dep.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+## Constants
+readonly PIP="9.0.3"
+readonly ANSIBLE="2.5.14"
+
+yum install -y epel-release git 
+yum install -y python2-pip
+
+python -m pip install --disable-pip-version-check --upgrade --force-reinstall \
+    pip==${PIP}
+python -m pip install --disable-pip-version-check --upgrade --force-reinstall \
+    setuptools
+python -m pip install --disable-pip-version-check --upgrade --force-reinstall \
+    pyOpenSSL \
+    requests \
+    netaddr \
+    jmespath \
+    ansible==${1-${ANSIBLE}}
